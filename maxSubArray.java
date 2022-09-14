ab6857417b80c289d71ea054beb5ac75f51f8681
opai elsheikh <72025112+opaielsheikh@users.noreply.github.com>
2022-09-14 20:35:25 +0100

    Maximum Subarray

---
+++ maxSubArray.java
@@ -0,0 +1,14 @@
+class MaxSubArray {
+    public int maxSubArray(int[] nums) {
+        int currentSubarray = nums[0];
+        int maxSubarray = nums[0];
+        
+        for(int i= 1; i < nums.length; i++){
+            int num = nums[i];
+            currentSubarray = Math.max(num, currentSubarray + num);
+            maxSubarray = Math.max(currentSubarray, maxSubarray);
+        }
+        
+        return maxSubarray;
+    }
+}
